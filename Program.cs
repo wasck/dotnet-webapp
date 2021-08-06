1be5135b3d3541b0e90db2a1d78deb141a6fae01
wasck <Waldemar.Schik@gmail.com>
2021-08-06 10:20:20 +0200

    feat: Add dotnet console app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace dotnet_webapp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine(JsonConvert.SerializeObject(args));
+    }
+  }
+}
